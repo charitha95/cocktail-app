fdec99f2775e5f40b0be875729dd4690bf165da1
Charitha Goonewardena <charitha.exe@gmail.com>
2023-06-02 15:15:08 +0530

    style: add header component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import FavoritesProvider from "./contexts/FavoriteDrinksContext";
 import AppRoutes from "./routes/AppRoutes";
+import Header from "./components/Header";
 
 function App(): JSX.Element {
   return (
     <ErrorBoundary>
       <FavoritesProvider>
-        <AppRoutes />
+        <BrowserRouter>
+          <Header />
+          <AppRoutes />
+        </BrowserRouter>
       </FavoritesProvider>
     </ErrorBoundary>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/branding/logo.svg";
+import classes from "./style.module.scss";
+
+export default function Header(): JSX.Element {
+  const links = [
+    { to: "/home", name: "Home" },
+    { to: "/favorites", name: "Favorites" }
+  ];
+
+  return (
+    <header className={classes.header}>
+      <NavLink to="/">
+        <img src={logo} alt="Cocktail Alchemy" />
+      </NavLink>
+      <div className={classes.links}>
+        {links.map((link, idx) => (
+          <NavLink
+            key={idx}
+            to={link.to}
+            className={({ isActive }) => (isActive ? `${classes.active}` : "")}
+          >
+            {link.name}
+          </NavLink>
+        ))}
+      </div>
+      <div>
+        <button>favs</button>
+      </div>
+    </header>
+  );
+}
