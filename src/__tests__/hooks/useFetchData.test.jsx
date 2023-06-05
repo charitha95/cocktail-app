12ee229d52a0eeccef094d799d0f699cd62f1904
Charitha Goonewardena <charitha.exe@gmail.com>
2023-06-06 02:45:02 +0530

    test: add useFetchData hooks test cases

--- src/__tests__/hooks/useFetchData.test.jsx
+++ src/__tests__/hooks/useFetchData.test.jsx
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import useFetchData from "../../hooks/useFetchData";
 import { describe, it, expect } from "vitest";
 
@@ -20,10 +20,61 @@ describe("useFetchData", () => {
 
   it("fetches data successfully", async () => {
     fetch.mockResponseOnce(JSON.stringify(mockData));
-
     const { result } = renderHook(() => useFetchData(""));
+
+    await waitFor(() => {
+      expect(result.current.data[0]).toEqual(mockData.drinks);
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.error).toBeNull();
+    });
+  });
+
+  it("handles fetch error", async () => {
+    const errorMessage = "Error message";
+    fetch.mockRejectOnce(new Error(errorMessage));
+    const { result } = renderHook(() => useFetchData(""));
+
+    await waitFor(() => {
+      expect(result.current.data).toEqual([]);
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.error).toBe(errorMessage);
+    });
+  });
+
+  it("fetches data multiple (5) times", async () => {
+    fetch
+      .mockResponseOnce(JSON.stringify(mockData))
+      .mockResponseOnce(JSON.stringify(mockData))
+      .mockResponseOnce(JSON.stringify(mockData))
+      .mockResponseOnce(JSON.stringify(mockData))
+      .mockResponseOnce(JSON.stringify(mockData));
+    const { result } = renderHook(() => useFetchData("", 5));
+
+    await waitFor(() => {
+      expect(result.current.data).toEqual([
+        mockData.drinks,
+        mockData.drinks,
+        mockData.drinks,
+        mockData.drinks,
+        mockData.drinks
+      ]);
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.error).toBeNull();
+    });
+  });
+
+  it("re-fetches data when calling fetchData", async () => {
+    fetch.mockResponseOnce(JSON.stringify(mockData));
+    const { result } = renderHook(() => useFetchData(""));
+
     await waitFor(() => {
       expect(result.current.data[0]).toEqual(mockData.drinks);
     });
+
+    act(() => {
+      result.current.fetchData();
+    });
+
+    expect(result.current.data[0]).toEqual(mockData.drinks);
   });
 });
